4407f8ab5cc3e9038796ecbf989afb1b1e97fca2
Belkius <118485347+Belkius@users.noreply.github.com>
2024-02-13 22:03:22 +0100

    Database Creation; post item endpoint;

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:postgres@localhost:5432'
+URL_DATABASE = ''
 
 engine = create_engine(URL_DATABASE)
 
