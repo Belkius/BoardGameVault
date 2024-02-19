7b4883143abbe96c4aeee8c874a532803fe89734
Belkius <118485347+Belkius@users.noreply.github.com>
2024-02-19 22:10:05 +0100

    2 api endpoints to get all ids and single ids from postgre DB

--- models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Boolean, Integer, String, DateTime, ForeignKey
-from database import Base
-
-class Games(Base):
-    __tablename__ = 'games'
-
-    id = Column(Integer, primary_key=True, index=True)
-    type = Column(String)
